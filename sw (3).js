1d6d747ba3a9a706a08a9828ed6b134c3efce624
nheskia <nheskia@gmail.com>
2026-06-20 14:36:43 -0400

    Add files via upload

---
+++ sw (3).js
@@ -0,0 +1,17 @@
+const CACHE = 'commute-v3';
+const ASSETS = ['./index.html', './manifest.json'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).then(r => {
+    const copy = r.clone();
+    caches.open(CACHE).then(c => c.put(e.request, copy));
+    return r;
+  }).catch(() => caches.match(e.request).then(r => r || caches.match('./index.html'))));
+});
